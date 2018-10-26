91414791041ccca5219ab64aa861ed15829fabdf
Veronica Sipos <veronicasipos@Veronicas-MacBook-Air.local>
2018-10-25 22:34:23 -0400

    completed assignment

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,32 +7,104 @@ let random=(Math.floor(Math.random()*101) + 19);
 $('#randomNumber').text(random);
 
 // Set Value to gems 
-let num1=(Math.floor(Math.random()*12)+1);
-let num2=(Math.floor(Math.random()*12)+1);
-let num3=(Math.floor(Math.random()*12)+1);
-let num4=(Math.floor(Math.random()*12)+1);
+let gem1 = (Math.floor(Math.random()*12)+1);
+let gem2 = (Math.floor(Math.random()*12)+1);
+let gem3 = (Math.floor(Math.random()*12)+1);
+let gem4 = (Math.floor(Math.random()*12)+1);
 
-// Linking gems to random numbers 
-$('#one').button
+console.log(random);
+console.log(gem1);
+console.log(gem2);
+console.log(gem3);
+console.log(gem4);
 
+// set wins, losses and total to 0 
 var userGuess = 0;
-var wins = 0;
-var losses = 0;
+$('#finalTotal').text(userGuess);
 
+var wins = 0;
 $('#numberWins').text(wins);
+
+var losses = 0;
 $('#numberLosses').text(losses);
 
-// set reset 
-function reset() {
-    Random=Math.floor(Math.random()*101+19);
-    console.log(Random)
-    $('#randomNumber').text(Random);
+ // reset game
+ function reset(){
+    random=(Math.floor(Math.random()*101) + 19);
+    console.log(random);
+    $('#randomNumber').text(random);
+    gem1 = (Math.floor(Math.random()*12)+1);
+    gem2 = (Math.floor(Math.random()*12)+1);
+    gem3 = (Math.floor(Math.random()*12)+1);
+    gem4 = (Math.floor(Math.random()*12)+1);
+    userGuess = 0;
+    $('#finalTotal').text(userGuess);
 }
 
-// set up clicks for buttons and conditions when pressed 
-$('#one').on('click', function (){
-    userTotal = userTotal + num1;
-    console.log("New userTotal= " + userTotal);
-    $('#finalTotal').text(userTotal);
+// Add wins to score
+function yay() {
+    wins++;
+    $('#numberWins').text(wins);
+    reset();
+}
+
+// Add losses to score
+function nay() { 
+    losses++;
+    $('#numberLosses').text(losses);
+    reset();
+}
+
+// create click function for gem1
+$('#one').click(function(){
+    userGuess = userGuess + gem1;
+    console.log("User Guess: " + userGuess);
+    $('#finalTotal').text(userGuess);
+    if (userGuess === random) {
+        yay();
+    }
+    else if (userGuess > random) {
+        nay();
+    }
+})
+
+// create click function for gem2
+$('#two').click(function(){
+    userGuess = userGuess + gem2;
+    console.log("User Guess: " + userGuess);
+    $('#finalTotal').text(userGuess);
+    if (userGuess === random) {
+        yay()
+    }
+    else if (userGuess > random) {
+        nay()
+    }
+})
+
+// create click function for gem3
+$('#three').click(function(){
+    userGuess = userGuess + gem3;
+    console.log("User Guess: " + userGuess);
+    $('#finalTotal').text(userGuess);
+    if (userGuess === random) {
+        yay()
+    }
+    else if (userGuess > random) {
+        nay()
+    }
+})
+
+// create click function for gem4
+$('#four').click(function(){
+    userGuess = userGuess + gem4;
+    console.log("User Guess: " + userGuess);
+    $('#finalTotal').text(userGuess);
+
+    if (userGuess === random) {
+        yay()
+    }
+    else if (userGuess > random) {
+        nay()
+    }
 })
 });
